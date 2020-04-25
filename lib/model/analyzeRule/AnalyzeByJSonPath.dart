9d6730334a29644e04c68d396ad8a724f5f1d18d
Celeter <alansky114@gmail.com>
2020-04-25 10:37:34 +0800

    修复AnalyzeByJsonPath.dart的一些bug

--- lib/model/analyzeRule/AnalyzeByJSonPath.dart
+++ lib/model/analyzeRule/AnalyzeByJSonPath.dart
@@ -1,8 +1,8 @@
 import 'package:jsonpath/json_path.dart';
 
 class AnalyzeByJSonPath {
-  final _jsonRulePattern = new RegExp(r"(?<={)\$\..+?(?=})");
-  dynamic _ctx = null;
+  final _jsonRulePattern = new RegExp(r'(?<={)\$\..+?(?=})');
+  dynamic _ctx = '';
 
   AnalyzeByJSonPath parse(json) {
     _ctx = json;
@@ -10,28 +10,28 @@ class AnalyzeByJSonPath {
   }
 
   String getString(String rule) {
-    if (rule.isEmpty) return "";
-    var result = "";
+    var result = '';
+    if (null == rule || rule.isEmpty) return result;
     List<String> rules;
     String elementsType;
-    if (rule.contains("&&")) {
-      rules = rule.split("&&");
-      elementsType = "&";
+    if (rule.contains('&&')) {
+      rules = rule.split('&&');
+      elementsType = '&';
     } else {
-      rules = rule.split("||");
-      elementsType = "|";
+      rules = rule.split('||');
+      elementsType = '|';
     }
     if (rules.length == 1) {
-      if (!rule.contains("{\$.")) {
+      if (!rule.contains('{\$.')) {
         try {
           var ob = JPath.compile(rule).search(_ctx);
+          if (null == ob) return result;
           if (ob is List) {
-            List<String> builder = [];
+            final builder = <String>[];
             for (var o in ob) {
-              builder.add(o);
-              builder.add("\n");
+              builder..add('$o')..add('\n');
             }
-            result = builder.toString().replaceFirst(new RegExp(r'\n$'), "");
+            result = builder.join('').replaceFirst(new RegExp(r'\n$'), '');
           } else {
             result = ob.toString();
           }
@@ -43,45 +43,46 @@ class AnalyzeByJSonPath {
         result = rule;
         var matcher = _jsonRulePattern.allMatches(rule);
         for (var m in matcher) {
-          result = result.replaceAll("{${m.group(0)}}", getString(m.group(0).trim()));
+          result = result.replaceAll(
+              '{${m.group(0)}}', getString(m.group(0).trim()));
         }
         return result;
       }
     } else {
-      List<String> textS = [];
+      final textS = <String>[];
       for (String rl in rules) {
-        String temp = getString(rl);
+        var temp = getString(rl);
         if (temp.isNotEmpty) {
-          textS.add(temp);
-          if (elementsType == "|") {
+          textS.add('$temp');
+          if ('|' == elementsType) {
             break;
           }
         }
       }
-      return textS.map((s) => s.trim()).join("\n");
+      return textS.map((s) => s.trim()).join('\n');
     }
   }
 
   List<String> getStringList(String rule) {
-    List<String> result = [];
-    if (rule.isEmpty) return result;
+    final result = <String>[];
+    if (null == rule || rule.isEmpty) return result;
     List<String> rules;
     String elementsType;
-    if (rule.contains("&&")) {
-      rules = rule.split("&&");
-      elementsType = "&";
-    } else if (rule.contains("%%")) {
-      rules = rule.split("%%");
-      elementsType = "%";
+    if (rule.contains('&&')) {
+      rules = rule.split('&&');
+      elementsType = '&';
+    } else if (rule.contains('%%')) {
+      rules = rule.split('%%');
+      elementsType = '%';
     } else {
-      rules = rule.split("||");
-      elementsType = "|";
+      rules = rule.split('||');
+      elementsType = '|';
     }
     if (rules.length == 1) {
-      if (!rule.contains("{\$.")) {
+      if (!rule.contains('{\$.')) {
         try {
           var object = JPath.compile(rule).search(_ctx);
-          if (object == null) return result;
+          if (null == object) return result;
           if (object is List) {
             for (var o in object) result.add(o.toString());
           } else {
@@ -96,28 +97,28 @@ class AnalyzeByJSonPath {
         for (var m in matcher) {
           var stringList = getStringList(m.group(0).trim());
           for (var s in stringList) {
-            result.add(rule.replaceAll("{${m.group(0)}}", s));
+            result.add(rule.replaceAll('{${m.group(0)}}', s));
           }
         }
         return result;
       }
     } else {
-      List<List<String>> results = [];
+      final results = <List<String>>[];
       for (var rl in rules) {
         List<String> temp = getStringList(rl);
-        if (temp.isNotEmpty) {
+        if (temp != null && temp.isNotEmpty) {
           results.add(temp);
-          if (temp.length > 0 && elementsType == "|") {
+          if (temp.length > 0 && '|' == elementsType) {
             break;
           }
         }
       }
       if (results.length > 0) {
-        if ("%" == elementsType) {
+        if ('%' == elementsType) {
           for (int i = 0; i < results[0].length; i++) {
             for (var temp in results) {
               if (i < temp.length) {
-                result.add(temp[i]);
+                result.add('${temp[i]}');
               }
             }
           }
@@ -131,45 +132,57 @@ class AnalyzeByJSonPath {
     }
   }
 
-  dynamic getObject(String rule) {
-    return JPath.compile(rule).search(_ctx);
+  Object getObject(String rule) {
+    try {
+      return JPath.compile(rule).search(_ctx);
+    } catch (e) {
+      print(e);
+      return '';
+    }
   }
 
-  List<dynamic> getList(String rule) {
-    List<dynamic> result = [];
-    if (rule.isEmpty) return result;
+  List<Object> getList(String rule) {
+    final result = <Object>[];
+    if (null == rule || rule.isEmpty) return result;
     String elementsType;
     List<String> rules;
-    if (rule.contains("&&")) {
-      rules = rule.split("&&");
-      elementsType = "&";
-    } else if (rule.contains("%%")) {
-      rules = rule.split("%%");
-      elementsType = "%";
+    if (rule.contains('&&')) {
+      rules = rule.split('&&');
+      elementsType = '&';
+    } else if (rule.contains('%%')) {
+      rules = rule.split('%%');
+      elementsType = '%';
     } else {
-      rules = rule.split("||");
-      elementsType = "|";
+      rules = rule.split('||');
+      elementsType = '|';
     }
     if (rules.length == 1) {
       try {
-        return JPath.compile(rules[0]).search(_ctx);
+        var res = JPath.compile(rules[0]).search(_ctx);
+        if (null == res) return result;
+//        print(res.runtimeType);
+        if (res[0] is List) {
+          res.forEach((r) => result.addAll(r));
+        } else {
+          result.addAll(res);
+        }
       } catch (e) {
         print(e);
-        return null;
       }
+      return result;
     } else {
-      List<List> results = [];
+      final results = <List<Object>>[];
       for (var rl in rules) {
         var temp = getList(rl);
-        if (temp.isNotEmpty) {
+        if (null != temp && temp.isNotEmpty) {
           results.add(temp);
-          if (temp.length > 0 && elementsType == "|") {
+          if (temp.length > 0 && '|' == elementsType) {
             break;
           }
         }
       }
       if (results.length > 0) {
-        if ("%" == elementsType) {
+        if ('%' == elementsType) {
           for (int i = 0; i < results[0].length; i++) {
             for (var temp in results) {
               if (i < temp.length) {
