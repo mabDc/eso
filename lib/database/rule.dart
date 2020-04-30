933a00f548450b9939c3e05de95a0e83819350ed
西红柿大芝麻 <747455334@qq.com>
2020-04-30 12:03:22 +0800

    更新rule实体

--- lib/database/rule.dart
+++ lib/database/rule.dart
@@ -2,18 +2,21 @@ import 'package:eso/api/api.dart';
 import 'package:uuid/uuid.dart';
 
 class Rule {
+  // 基本信息
   String id = Uuid().v4();
   int createTime = DateTime.now().microsecondsSinceEpoch;
   int modifiedTime = DateTime.now().microsecondsSinceEpoch;
-  bool enable = true;
   String name = '';
   String host = '';
   ContentType contentType = ContentType.MANGA;
-  bool useCheerio = false;
+  // bool useCheerio = false;
   bool useCryptoJS = false;
   bool useMultiRoads = false;
+  String loadJs = '';
+  String userAgent = '';
 
   // 发现规则
+  bool enableDiscover = true;
   String discoverUrl = '';
   String discoverItems = '';
 
@@ -26,10 +29,11 @@ class Rule {
   String discoverDescription = '';
   String discoverResult = '';
 
+  // 搜索规则
+  bool enableSearch = true;
   String searchUrl = '';
   String searchItems = '';
 
-  // 搜索规则
   String searchList = '';
   String searchTags = '';
   String searchName = '';
@@ -39,53 +43,37 @@ class Rule {
   String searchDescription = '';
   String searchResult = '';
 
-  String detailUrl = '';
-  String detailItems = '';
+  // 章节规则
+  String chapterRoads = '';
+  String chapterRoadName = '';
   String chapterUrl = '';
   String chapterItems = '';
+
+  String chapterName = '';
+  String chapterCover = '';
+  String chapterLock = '';
+  String chapterTime = '';
+  String chapterResult = '';
+
+  // 正文规则
   String contentUrl = '';
   String contentItems = '';
-
-  String road; //List<dynamic>
-  String roadName;
-  String roadElement; // chapterList
-  String chapterName;
-  String chapterResult;
-
-  Rule(
-    this.id,
-    this.enable,
-    this.name,
-    this.host,
-    this.contentType,
-    this.useCheerio,
-    this.useCryptoJS,
-    this.useMultiRoads,
-    this.discoverUrl,
-    this.discoverItems,
-    this.searchUrl,
-    this.searchItems,
-    this.detailUrl,
-    this.detailItems,
-    this.chapterUrl,
-    this.chapterItems,
-    this.contentUrl,
-    this.contentItems,
-  );
 
   Rule.newRule() {
     id = Uuid().v4();
     createTime = DateTime.now().microsecondsSinceEpoch;
     modifiedTime = DateTime.now().microsecondsSinceEpoch;
-    enable = true;
     name = '';
     host = '';
     contentType = ContentType.MANGA;
-    useCheerio = false;
+    // bool useCheerio = false;
     useCryptoJS = false;
     useMultiRoads = false;
+    loadJs = '';
+    userAgent = '';
 
     // 发现规则
+    enableDiscover = true;
     discoverUrl = '';
     discoverItems = '';
 
@@ -98,10 +86,11 @@ class Rule {
     discoverDescription = '';
     discoverResult = '';
 
+    // 搜索规则
+    enableSearch = true;
     searchUrl = '';
     searchItems = '';
 
-    // 搜索规则
     searchList = '';
     searchTags = '';
     searchName = '';
@@ -111,60 +100,35 @@ class Rule {
     searchDescription = '';
     searchResult = '';
 
-    detailUrl = '';
-    detailItems = '';
+    // 章节规则
+    chapterRoads = '';
+    chapterRoadName = '';
     chapterUrl = '';
     chapterItems = '';
+
+    chapterName = '';
+    chapterCover = '';
+    chapterLock = '';
+    chapterTime = '';
+    chapterResult = '';
+
+    // 正文规则
     contentUrl = '';
     contentItems = '';
-
-    road = ''; //List<dynamic>
-    roadName = '';
-    roadElement = ''; // chapterList
-    chapterName = '';
-    chapterResult = '';
   }
 
+  // Todo: 自动生成Rule构造方法
+  Rule(
+      //,
+      );
+
+  // Todo: 补全fromJson
   Rule.fromJson(Map<dynamic, dynamic> json) {
     final defaultRule = Rule.newRule();
     id = json['id'] ?? defaultRule.id;
-    enable = json['enable'] ?? defaultRule.enable;
-    name = json['name'] ?? defaultRule.name;
-    host = json['host'] ?? defaultRule.host;
-    contentType = json['contentType'] ?? defaultRule.contentType;
-    useCheerio = json['useCheerio'] ?? defaultRule.useCheerio;
-    useCryptoJS = json['useCryptoJS'] ?? defaultRule.useCryptoJS;
-    useMultiRoads = json['useMultiRoads'] ?? defaultRule.useMultiRoads;
-    discoverUrl = json['discoverUrl'] ?? defaultRule.discoverUrl;
-    discoverItems = json['discoverItems'] ?? defaultRule.discoverItems;
-    searchUrl = json['searchUrl'] ?? defaultRule.searchUrl;
-    searchItems = json['searchItems'] ?? defaultRule.searchItems;
-    detailUrl = json['detailUrl'] ?? defaultRule.detailUrl;
-    detailItems = json['detailItems'] ?? defaultRule.detailItems;
-    chapterUrl = json['chapterUrl'] ?? defaultRule.chapterUrl;
-    chapterItems = json['chapterItems'] ?? defaultRule.chapterItems;
-    contentUrl = json['contentUrl'] ?? defaultRule.contentUrl;
-    contentItems = json['contentItems'] ?? defaultRule.contentItems;
   }
-
+  // Todo: 补全toJson
   Map<dynamic, dynamic> toJson() => {
         'id': id,
-        'enable': enable,
-        'name': name,
-        'host': host,
-        'contentType': contentType,
-        'useCheerio': useCheerio,
-        'useCryptoJS': useCryptoJS,
-        'useMultiRoads': useMultiRoads,
-        'discoverUrl': discoverUrl,
-        'discoverItems': discoverItems,
-        'searchUrl': searchUrl,
-        'searchItems': searchItems,
-        'detailUrl': detailUrl,
-        'detailItems': detailItems,
-        'chapterUrl': chapterUrl,
-        'chapterItems': chapterItems,
-        'contentUrl': contentUrl,
-        'contentItems': contentItems,
       };
 }
