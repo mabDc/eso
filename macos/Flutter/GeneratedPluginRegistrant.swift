d14a0f03ffe3936b9687683e6a48ef3790c1489f
西红柿大芝麻 <747455334@qq.com>
2020-05-05 15:03:31 +0800

    fix

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import video_player
 import audioplayers
-import flutter_js
 import path_provider_macos
 import shared_preferences_macos
+import sqflite
 import url_launcher_macos
-import video_player
+import flutter_js
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FLTVideoPlayerPlugin"))
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
-  SwiftFlutterJsPlugin.register(with: registry.registrar(forPlugin: "SwiftFlutterJsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FLTVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FLTVideoPlayerPlugin"))
+  SwiftFlutterJsPlugin.register(with: registry.registrar(forPlugin: "SwiftFlutterJsPlugin"))
 }
--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,8 +4,9 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
+    let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(NSMakeRect(self.frame.origin.x,self.frame.origin.y, 400, 800), display: true)
+    self.setFrame(windowFrame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
