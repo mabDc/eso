207e86b692df96ca7535624c995b3701d3825e51
西红柿大芝麻 <747455334@qq.com>
2020-05-14 12:00:30 +0800

    fix

--- lib/utils/input_stream.dart
+++ lib/utils/input_stream.dart
@@ -3,12 +3,15 @@ import 'package:fast_gbk/fast_gbk.dart';
 
 class InputStream {
   static String autoDecode(List<int> bytes) {
-    final str = latin1.decode(bytes);
+    final str = utf8.decode(bytes);
     if (!str.contains('charset')) return str;
     try {
       final charset = RegExp(r"""(?<=charset\s*\=\s*["']?)[^"';\s]+""")
           .firstMatch(str)
           .group(0);
+      if (charset.contains(RegExp("utf-?8", caseSensitive: false))) {
+        return str;
+      }
       return decode(bytes, charset);
     } catch (e) {
       print(e);
@@ -18,6 +21,6 @@ class InputStream {
 
   static String decode(List<int> bytes, [String charset = "utf-8"]) {
     if (charset.toLowerCase().startsWith('gb')) return gbk.decode(bytes);
-    return (Encoding.getByName(charset) ?? latin1).decode(bytes);
+    return (Encoding.getByName(charset) ?? utf8).decode(bytes);
   }
 }
