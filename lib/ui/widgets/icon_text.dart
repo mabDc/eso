feae2b0d0f0cdd822cea4a313fcb20abb6c12739
yangyxd <yangyxd@126.com>
2020-07-02 10:05:55 +0800

    .

--- lib/ui/widgets/icon_text.dart
+++ lib/ui/widgets/icon_text.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
 
+/// icon text
 class IconText extends StatelessWidget {
   final String text;
   final Icon icon;
   final double iconSize;
+  final Axis direction;
+  /// icon padding
   final EdgeInsetsGeometry padding;
   final TextStyle style;
   final int maxLines;
   final bool softWrap;
   final TextOverflow overflow;
+  final TextAlign textAlign;
+
   const IconText(this.text,
       {Key key,
       this.icon,
       this.iconSize,
+      this.direction = Axis.horizontal,
       this.style,
       this.maxLines,
       this.softWrap,
       this.padding,
+      this.textAlign,
       this.overflow = TextOverflow.ellipsis})
-      : super(key: key);
+      : assert(direction != null),
+        assert(overflow != null),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return icon == null
         ? Text(text ?? '', style: style)
         : text == null || text.isEmpty
-            ? icon
+            ? (padding == null ? icon : Padding(padding: padding, child: icon))
             : RichText(
                 text: TextSpan(style: style, children: [
                   WidgetSpan(
@@ -36,16 +45,20 @@ class IconText extends StatelessWidget {
                                 ? 16
                                 : style.fontSize + 1),
                         color: style == null ? null : style.color),
-                    child: padding == null ? icon : Padding(
-                      padding: padding,
-                      child: icon,
-                    ),
+                    child: padding == null
+                        ? icon
+                        : Padding(
+                            padding: padding,
+                            child: icon,
+                          ),
                   )),
-                  TextSpan(text: text),
+                  TextSpan(
+                      text: direction == Axis.horizontal ? text : "\n$text"),
                 ]),
                 maxLines: maxLines,
                 softWrap: softWrap ?? true,
                 overflow: overflow ?? TextOverflow.clip,
+                textAlign: textAlign ?? (direction == Axis.horizontal ? TextAlign.start : TextAlign.center),
               );
   }
 }
