367089448d43733694b592e7729fde61066a7a0b
mabdc <747455334@qq.com>
2020-09-13 02:22:21 +0800

    嗅探

--- lib/api/analyzer_filter.dart
+++ lib/api/analyzer_filter.dart
@@ -1,23 +1,22 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:eso/database/rule.dart';
 import 'package:flutter_webview/flutter_webview.dart';
 
 import 'analyzer.dart';
 
-Future<dynamic> webview(String url, Map options) async {
+Future<dynamic> webview(String url, int duration, Map options) async {
   Completer c = new Completer();
   var webview = FlutterWebview();
   await webview.setMethodHandler((String method, dynamic args) async {
-    print("$method($args)");
     if (method == "onNavigationCompleted") {
-      await Future.delayed(Duration(seconds: 10));
+      await Future.delayed(Duration(seconds: duration));
       if (!c.isCompleted)
         c.completeError("Webview Call timeout 10 seconds after page completed.");
     }
     var callback = options[method];
-    if (callback != null) if ((await callback([args])) == true) {
-      print(args);
+    if (callback != null) if ((await callback(args)) == true) {
       if (!c.isCompleted) c.complete(args);
     }
     return;
@@ -58,26 +57,41 @@ class AnalyzerFilter implements Analyzer {
   }
 
   @override
-  Future<String> getElements(String rule) {
-    return getString(rule);
+  Future<List<String>> getElements(String rule) {
+    return getStringList(rule);
   }
 
   @override
-  Future<String> getString(String rule) async {
-    final r = await webview(url, {
+  Future<List<String>> getString(String rule) async {
+    return getStringList(rule);
+  }
+
+  String covertHeaders(Map args) {
+    args["headers"] = Map.fromIterable(
+      args["headers"],
+      key: (e) => (e as Map).keys.first,
+      value: (e) => (e as Map).values.first,
+    );
+    return jsonEncode(args);
+  }
+
+  @override
+  Future<List<String>> getStringList(String rule) async {
+    List<String> result = <String>[];
+    final r = rule.split("@@");
+    final duration = r.length > 1 ? int.parse(r[1]) : 10;
+    await webview(url, duration, {
       "ua": _rule.userAgent.trim().isNotEmpty
           ? _rule.userAgent
           : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
-      "onRequest": (o) {
-        if (o["url"].includes(RegExp(rule))) return true;
+      "onRequest": (args) {
+        if ((args["url"] as String).contains(RegExp(r[0]))) {
+          result.add(covertHeaders(args));
+          return true;
+        }
         return false;
       }
     });
-    return null == r ? "" : "$r";
-  }
-
-  @override
-  Future<String> getStringList(String rule) {
-    return getString(rule);
+    return result;
   }
 }
