56ecb539c961c23ba6b0894abc811b3d12aaf7d7
mabdc <747455334@qq.com>
2020-09-17 04:29:11 +0800

    增加刷新

--- lib/database/search_item_manager.dart
+++ lib/database/search_item_manager.dart
@@ -1,5 +1,8 @@
 import 'dart:convert';
 
+import 'package:eso/api/api_manager.dart';
+import 'package:oktoast/oktoast.dart' as oktoast;
+
 import 'chapter_item.dart';
 import 'search_item.dart';
 import '../global.dart';
@@ -55,16 +58,14 @@ class SearchItemManager {
   }
 
   static List<ChapterItem> getChapter(int id) {
-    return LocalStorage
-        .getStringList(genChapterKey(id))
+    return LocalStorage.getStringList(genChapterKey(id))
         .map((item) => ChapterItem.fromJson(jsonDecode(item)))
         .toList();
   }
 
   static void initSearchItem() {
     _searchItem = <SearchItem>[];
-    LocalStorage
-        .getStringList(key)
+    LocalStorage.getStringList(key)
         ?.forEach((item) => _searchItem.add(SearchItem.fromJson(jsonDecode(item))));
   }
 
@@ -89,8 +90,7 @@ class SearchItemManager {
   }
 
   static Future<String> backupItems() async {
-    if (_searchItem == null || _searchItem.isEmpty)
-      initSearchItem();
+    if (_searchItem == null || _searchItem.isEmpty) initSearchItem();
     String s = json.encode(_searchItem.map((item) {
       Map<String, dynamic> json = item.toJson();
       json["chapters"] =
@@ -114,6 +114,55 @@ class SearchItemManager {
     });
     saveSearchItem();
     return true;
+  }
+
+  static Future<void> refreshAll() async {
+    // 先用单并发，加延时5s判定
+    for (var item in _searchItem) {
+      var current = item.name;
+      await Future.any([
+        refreshItem(item),
+        (SearchItem temp) async {
+          await Future.delayed(Duration(seconds: 5), () {
+            if (current == temp.name) showToast("${temp.name} 章节更新超时");
+          });
+        }(item),
+      ]);
+      current = null;
+    }
+    return;
+  }
+
+  static void showToast(String msg) {
+    oktoast.showToast(msg, position: oktoast.ToastPosition.bottom);
+  }
+
+  static Future<void> refreshItem(SearchItem item) async {
+    if (item.chapters.isEmpty) {
+      item.chapters = SearchItemManager.getChapter(item.id);
+    }
+    List<ChapterItem> chapters;
+    try {
+      chapters = await APIManager.getChapter(item.originTag, item.url);
+    } catch (e) {
+      showToast("${item.name} 章节获取失败");
+      return;
+    }
+    if (chapters.isEmpty) {
+      showToast("${item.name} 章节为空");
+      return;
+    }
+    final newCount = item.chaptersCount - item.chapters.length;
+    if (newCount > 0) {
+      showToast("${item.name} 新增 $newCount 章节");
+      item.chapters = chapters;
+      item.chapter = item.chapters.last?.name;
+      item.chaptersCount = item.chapters.length;
+      await SearchItemManager.saveChapter(item.id, item.chapters);
+    } else {
+      showToast("${item.name} 无新增章节");
+    }
+    return;
   }
 }
 
