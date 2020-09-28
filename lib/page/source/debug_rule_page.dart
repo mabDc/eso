bc058172b47acb68739c35979b9d5908f6065748
mabdc <747455334@qq.com>
2020-09-28 15:30:56 +0800

    up

--- lib/page/source/debug_rule_page.dart
+++ lib/page/source/debug_rule_page.dart
@@ -7,12 +7,8 @@
  * @FilePath: \eso\lib\page\source\debug_rule_page.dart
  */
 import 'package:eso/database/rule.dart';
-import 'package:eso/model/debug_rule_provider.dart';
 import 'package:eso/ui/edit/edit_view.dart';
-import 'package:eso/ui/widgets/draggable_scrollbar_sliver.dart';
-import 'package:eso/ui/widgets/keyboard_dismiss_behavior_view.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../../fonticons_icons.dart';
 
@@ -23,47 +19,24 @@ class DebugRulePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textColor = Theme.of(context).textTheme.bodyText1.color;
-    return ChangeNotifierProvider<DebugRuleProvider>(
-      create: (_) => DebugRuleProvider(rule, textColor),
-      builder: (context, child) => Scaffold(
-        appBar: AppBar(
-          title: _buildTextField(
-            context,
-            Provider.of<DebugRuleProvider>(context, listen: false).search,
+    return Scaffold(
+      appBar: AppBar(
+        title: _buildTextField(
+          context,
+          (_) {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(FIcons
+                .compass), // Text("发现测试",style: TextStyle(color: Theme.of(context).textTheme.bodyText1.color)),
+            tooltip: "发现测试",
+            onPressed: () {},
           ),
-          actions: [
-            IconButton(
-              icon: Icon(FIcons
-                  .compass), // Text("发现测试",style: TextStyle(color: Theme.of(context).textTheme.bodyText1.color)),
-              tooltip: "发现测试",
-              onPressed: Provider.of<DebugRuleProvider>(context, listen: false).discover,
-            ),
-          ],
-        ),
-        body: Consumer<DebugRuleProvider>(
-          builder: (context, DebugRuleProvider provider, _) {
-            if (provider.rows.isEmpty) {
-              return Center(
-                child: Icon(FIcons.cpu,
-                    size: 128,
-                    color: Theme.of(context).primaryColorDark.withOpacity(0.08)),
-              );
-            }
-            return KeyboardDismissBehaviorView(
-              child: DraggableScrollbar.semicircle(
-                controller: provider.controller,
-                child: ListView.builder(
-                  controller: provider.controller,
-                  padding: EdgeInsets.all(8),
-                  itemCount: provider.rows.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return provider.rows[index];
-                  },
-                ),
-              ),
-            );
-          },
-        ),
+        ],
+      ),
+      body: Center(
+        child: Icon(FIcons.cpu,
+            size: 128, color: Theme.of(context).primaryColorDark.withOpacity(0.08)),
       ),
     );
   }
