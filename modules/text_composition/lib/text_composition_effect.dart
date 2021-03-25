0d02022ae76186bfc9c8b0b4cf3de027151500e9
mabdc <747455334@qq.com>
2021-03-25 12:28:49 +0800

    curl和textpaint 做成独立方法

--- modules/text_composition/lib/text_composition_effect.dart
+++ modules/text_composition/lib/text_composition_effect.dart
@@ -30,8 +30,8 @@ class TextCompositionEffect extends CustomPainter {
       // 画正好加载最后章节
       return;
     }
-    if (index > textComposition.currentIndex + 1) return;
-    if (index < textComposition.currentIndex - 1) return;
+    if (index > textComposition.currentIndex + 2) return;
+    if (index < textComposition.currentIndex - 2) return;
 
     final pos = amount.value;
     if (pos < 0.004) return;
@@ -57,59 +57,58 @@ class TextCompositionEffect extends CustomPainter {
         image = value;
         drawing = false;
       });
-      if (pos > 0.996) {
-        canvas.drawPicture(picture!);
-      }
-      return;
     }
 
-    if (pos > 0.996) {
-      if (picture != null) {
-        canvas.drawPicture(picture!);
-      }
-      return;
-    }
-
-    final posP = pos * size.width;
-    if (config.animation == 'cover') {
-      canvas.translate(posP - size.width, 0);
+    if (config.animation == 'cover' && picture != null) {
+      canvas.translate(pos * size.width - size.width, 0);
       canvas.drawPicture(picture!);
     } else if (config.animation == 'curl') {
-      final movX = (1.0 - pos) * 0.85;
-      final calcR = (movX < 0.20) ? radius * movX * 5 : radius;
-      final wHRatio = 1 - calcR;
-      final hWRatio = image!.height / image!.width;
-      final hWCorrection = (hWRatio - 1.0) / 2.0;
-
-      final w = size.width.toDouble();
-      final h = size.height.toDouble();
-      final c = canvas;
-      final shadowXf = (wHRatio - movX);
-      final shadowSigma = Shadow.convertRadiusToSigma(8.0 + (32.0 * (1.0 - shadowXf)));
-      final pageRect = Rect.fromLTRB(0.0, 0.0, w * shadowXf, h);
-      c.drawRect(pageRect, Paint()..color = config.backgroundColor);
-      if (pos != 0) {
-        c.drawRect(
-          pageRect,
-          Paint()
-            ..color = Colors.black54
-            ..maskFilter = MaskFilter.blur(BlurStyle.outer, shadowSigma),
-        );
+      if (pos > 0.996 && picture != null) {
+        canvas.drawPicture(picture!);
+      } else if (image != null) {
+        paintCurl(canvas, size, pos, image!, config.backgroundColor);
       }
+    }
+  }
 
-      final ip = Paint();
-      for (double x = 0; x < size.width; x++) {
-        final xf = (x / w);
-        final v =
-            (calcR * (math.sin(math.pi / 0.5 * (xf - (1.0 - pos)))) + (calcR * 1.1));
-        final xv = (xf * wHRatio) - movX;
-        final sx = (xf * image!.width);
-        final sr = Rect.fromLTRB(sx, 0.0, sx + 1.0, image!.height.toDouble());
-        final yv = ((h * calcR * movX) * hWRatio) - hWCorrection;
-        final ds = (yv * v);
-        final dr = Rect.fromLTRB(xv * w, 0.0 - ds, xv * w + 1.0, h + ds);
-        c.drawImageRect(image!, sr, dr, ip);
-      }
+  /// 原始动效
+  void paintCurl(ui.Canvas canvas, ui.Size size, double pos, ui.Image image,
+      Color? backgroundColor) {
+    final movX = (1.0 - pos) * 0.85;
+    final calcR = (movX < 0.20) ? radius * movX * 5 : radius;
+    final wHRatio = 1 - calcR;
+    final hWRatio = image.height / image.width;
+    final hWCorrection = (hWRatio - 1.0) / 2.0;
+
+    final w = size.width.toDouble();
+    final h = size.height.toDouble();
+    final c = canvas;
+    final shadowXf = (wHRatio - movX);
+    final shadowSigma = Shadow.convertRadiusToSigma(8.0 + (32.0 * (1.0 - shadowXf)));
+    final pageRect = Rect.fromLTRB(0.0, 0.0, w * shadowXf, h);
+    if (backgroundColor != null) {
+      c.drawRect(pageRect, Paint()..color = backgroundColor);
+    }
+    if (pos != 0) {
+      c.drawRect(
+        pageRect,
+        Paint()
+          ..color = Colors.black54
+          ..maskFilter = MaskFilter.blur(BlurStyle.outer, shadowSigma),
+      );
+    }
+
+    final ip = Paint();
+    for (double x = 0; x < size.width; x++) {
+      final xf = (x / w);
+      final v = (calcR * (math.sin(math.pi / 0.5 * (xf - (1.0 - pos)))) + (calcR * 1.1));
+      final xv = (xf * wHRatio) - movX;
+      final sx = (xf * image.width);
+      final sr = Rect.fromLTRB(sx, 0.0, sx + 1.0, image.height.toDouble());
+      final yv = ((h * calcR * movX) * hWRatio) - hWCorrection;
+      final ds = (yv * v);
+      final dr = Rect.fromLTRB(xv * w, 0.0 - ds, xv * w + 1.0, h + ds);
+      c.drawImageRect(image, sr, dr, ip);
     }
   }
 
