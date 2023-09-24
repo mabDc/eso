ad47e8af53f3ef9d649e13b2b1dc7f3046b62c9a
mabdc <747455334@qq.com>
2023-09-24 10:28:26 +0800

    fix

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,7 +15,7 @@ import just_audio
 import package_info_plus_macos
 import path_provider_foundation
 import share_plus_macos
-import shared_preferences_foundation
+import shared_preferences_macos
 import sqflite
 import url_launcher_macos
 import window_manager
